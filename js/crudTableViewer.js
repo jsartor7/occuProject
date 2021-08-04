c4474b8081e1edce6cad0aa4005d085f69c98191
jsartor7 <jsartor7@uoregon.edu>
2021-08-04 16:44:22 -0700

    compare functionality

--- js/crudTableViewer.js
+++ js/crudTableViewer.js
@@ -11,11 +11,15 @@ class crudTableViewer{
     }
 
 
-    refreshTableData() {
-        this.tableData = tableManager.requestTableData();
+    redrawTable() {
         this.clearTable();
         this.generateTableHead();
         this.generateTable();
+    }
+    
+    refreshTableData() {
+        this.tableData = tableManager.requestTableData();
+        this.redrawTable();
     }
     
     clearTable() {
@@ -89,6 +93,8 @@ class crudTableViewer{
             let compCheckbox = document.createElement("input");
             compCheckbox.setAttribute("type", "checkbox");
             compCheckbox.setAttribute("id", "compare"+"="+rowNum);
+            function onClick() { tableViewer.compare(); }
+            compCheckbox.addEventListener('change', onClick, false);
             compCell.appendChild(compCheckbox);
             
             //add in all the data
@@ -199,6 +205,82 @@ class crudTableViewer{
 
         
     }
+    
+    compare() {
+    
+        var tableElem = document.getElementById("crudTable");
+        var tableRows = tableElem.children[0].childNodes;
+        //start at 1 to not search the header row
+        
+        var hideNameList = [];
+        var compareList = [];
+        
+        var numComparing = 0;
+        for (let i = 2; i < tableRows.length; i++) {
+            
+            var myRow = tableRows[i];
+            var isChecked = myRow.childNodes[1].childNodes[0].checked
+            if (isChecked) {
+                numComparing +=1;
+                compareList.push(myRow)
+            }
+            else {
+                hideNameList.push(myRow.childNodes[2].childNodes[0].value);
+            }
+        }
+
+        //only compare sets of size 2
+        if (numComparing == 2){
+            
+            
+            
+            
+            //i=2 --> skip the header and search rows
+            for (let i = 2; i < tableRows.length; i++) {
+                var myRow = tableRows[i];
+                var myName = myRow.childNodes[2].childNodes[0].value
+                if (hideNameList.includes(myName)) {
+                    myRow.hidden = true;
+                }
+                else {
+                    myRow.hidden = false;
+                }
+            }
+            
+            for (let i = 0; i < 2; i++) {
+                for (let j = 3; j<6; j++) {
+                    if (compareList[i].childNodes[j].childNodes[0].value == compareList[(i+1)%2].childNodes[j].childNodes[0].value) {
+                    }
+                    else {
+                        compareList[i].childNodes[j].bgColor ="red";
+                    }
+                }
+            }
+            
+            //but we also are going to manually hide the search row
+            //since searching and comparing simultanously is nonsense
+            
+            //this might not be the desired result, i could also imagine that maybe the user wants to still see search results after de-comparing... but i figure this is OK for now.
+            for (let i = 2; i < 5; i++) {
+                tableRows[1].childNodes[i].childNodes[0].value="";
+            }
+            tableRows[1].hidden = true;
+
+        }
+        else {
+            //redraw table if the search bar is hidden.
+            //this is a little counter-intuitive, but if the search bar was hidden then either:
+            //1: you weren't searching in the first place, or
+            //2: the search bar was hidden from comparing.
+            //OTOH, if the search bar *wasn't* hidden, you shouldn't unhide anything because it wasn't hidden on account of "comparing"
+            if (tableRows[1].hidden) {
+                //you could obviously just unhide the rows, but then you'd have to unhighlight the red cells, which is mildly annoying because the bg colors alternate
+                this.redrawTable()
+            }
+        }
+        
+    }
+    
 }
 
 
